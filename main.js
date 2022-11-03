eae0b890fd3f8444e426c0abb538b80bb44e2374
Faseeh Ahmed <faseeh_458@hotmail.com>
2022-11-03 14:22:29 +0000

    On Step 3

--- main.js
+++ main.js
@@ -1,5 +1,26 @@
 /* 
-👉 Write your kata here!
+👉 Write your kata here
+
+Write a function that takes an array of numbers and returns the sum of the numbers. The numbers can be negative or non-integer. If the array does not contain any numbers then you should return 0. 
+
+You can assume that you are only given numbers.
+
+Examples:
+Input: [1, 5.2, 4, 0, -1]
+Output: 9.2
+
+Input: []
+Output: 0
+
+Input: [-2.398]
+Output: -2.398
 */
 
 //👉 Write the function your CodeWarriors will start with below here:
+export default function sumTester(arr){
+    let count = 0;
+    for(let i=0; i<arr.length; i++){
+      count+=arr[i]
+    }
+    return count
+}
--- main.test.js
+++ main.test.js
@@ -1,1 +1,40 @@
 //👉 Write your tests below here:
+
+// import the Express app
+import sumTester from './main.js';
+// import what we need from jest (expect, test)
+
+import { expect, test } from '@jest/globals';
+
+test('Positive and Negative sums', function(){
+    let arr = [1, 5.2, 4, 0, -1]
+    let actual = sumTester(arr)
+    let expected = 9.2
+    expect(actual).toEqual(expected)
+})
+
+/*
+test 1
+let arr = [1, 5.2, 4, 0, -1]
+let actual = sumTester(arr)
+let expected = 9.2
+expect(actual).toEqual(expected)
+
+test 2 
+let arr = [0, 0, 0, 0,0 ]
+let actual = sumTester(arr)
+let expected = 0
+expect(actual).toEqual(expected)
+
+test 3 
+let arr= []
+let actual = sumTester(arr)
+let expected = 0
+expect(actual).toEqual(expected)
+
+**test 4**
+let arr= [-5.5]
+let actual = sumTester(arr)
+let expected = -5.5
+expect(actual).toEqual(expected)
+*/
