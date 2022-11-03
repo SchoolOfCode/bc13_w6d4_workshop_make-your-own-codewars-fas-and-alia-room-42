3a2abec0f3dfd3733f80f5707b2de40400b165bd
Faseeh Ahmed <faseeh_458@hotmail.com>
2022-11-03 15:47:45 +0000

    Solutions removed

--- main.js
+++ main.js
@@ -1,5 +1,4 @@
 /* 
-👉 Write your kata here
 
 Write a function that takes an array of numbers and returns the sum of the numbers. The numbers can be negative or non-integer. If the array does not contain any numbers then you should return 0. 
 
@@ -16,11 +15,7 @@ Input: [-2.398]
 Output: -2.398
 */
 
-//👉 Write the function your CodeWarriors will start with below here:
+
 export default function sumTester(arr) {
-	let count = 0;
-	for (let i = 0; i < arr.length; i++) {
-		count += arr[i];
-	}
-	return count;
+	
 }
